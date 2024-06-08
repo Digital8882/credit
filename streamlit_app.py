19f9ff06f715f2215c624361dee4aab54ead4a44
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-08 10:00:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ SENDER_PASSWORD = 'Lovelife1#'
 
 # Environment variables for Langsmith
 os.environ["LANGSMITH_TRACING_V2"] = "true"
-os.environ["LANGSMITH_PROJECT"] = "SL0ll05d670o"
+os.environ["LANGSMITH_PROJECT"] = "SL0ll05gfd670o"
 os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGSMITH_API_KEY"] = "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"
 

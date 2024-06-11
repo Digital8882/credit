3a27de94a9f7da909cf17efa1c9a4501d053cb5c
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-11 02:17:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -376,7 +376,7 @@ async def start_crew_process(email, product_service, price, currency, payment_fr
     project_crew = Crew(
         tasks=[new_task, icp_task, channels_task, pains_task, gains_task, jtbd_task, propdesign_task, customerj_task, methodology_task],
         agents=[researcher, product_manager, marketing_director, sales_director],
-        manager_llm=ChatOpenAI(temperature=0, model="gpt-4o"),
+        manager_llm=ChatOpenAI(temperature=0.2, model="gpt-4o"),
         max_rpm=3,
         process=Process.hierarchical,
         memory=True,

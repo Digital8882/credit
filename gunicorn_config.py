478e17315b1891f6e3efc6eb39f90e176304c1ca
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-13 06:24:30 +0200

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,22 @@
+# gunicorn_config.py
+
+# Number of worker processes
+workers = 1
+
+# The timeout in seconds for requests
+timeout = 120
+
+# Number of worker connections
+worker_connections = 1000
+
+# Log level
+loglevel = 'debug'
+
+# Enable or disable the use of the HTTP Keep-Alive feature
+keepalive = 2
+
+# The maximum number of requests a worker will process before restarting
+max_requests = 1000
+
+# Random jitter added to max_requests to prevent all workers from restarting at the same time
+max_requests_jitter = 100

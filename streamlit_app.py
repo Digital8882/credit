6e3199b3a882be91758295ad2af90c46e899dc53
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-13 04:28:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
+from memory_profiler import profile
+import logging
 import streamlit as st
 import requests
 import os
-from memory_profiler import profile
+
 
 # Set the backend URL directly
 backend_url = "https://credit-t9kr.onrender.com"
